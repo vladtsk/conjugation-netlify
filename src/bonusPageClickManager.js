dc19421cb9c44112dd45bba6cb9846b86f346628
Vlad <vlad.tsk150@gmail.com>
2024-09-15 17:53:00 +0200

    Updating the bonuses

--- src/bonusPageClickManager.js
+++ src/bonusPageClickManager.js
@@ -1,40 +1,67 @@
 // Manage menu clicks on the bonus page
 
 export function manageBonusMenuClicks(userId, subStatus) {
-    const bonus1Menu = document.querySelector(".bonus1Menu");
-    const bonus1SubContainer = document.querySelector(".bonus1-subcontainer");
+  const bonus1Menu = document.querySelector(".bonus1Menu");
+  const bonus1SubContainer = document.querySelector(".bonus1-subcontainer");
 
-    const bonus2Menu = document.querySelector(".bonus2Menu");
-    const bonus2SubContainer = document.querySelector(".bonus2-subcontainer");
-    
-    bonus1Menu.addEventListener("click", ()=>{
-      if(bonus1SubContainer.style.display === "none") {
-          bonus1SubContainer.style.display = "block";
-          bonus1Menu.innerHTML = `<div><i class="fa-solid fa-book"></i>Bonus 1: Conjugation guide</div>
-          <i class='fa-solid fa-circle-chevron-up'></i>`
-  
-          
-  
-      } else {
-          bonus1SubContainer.style.display = "none";
-          bonus1Menu.innerHTML = `<div><i class="fa-solid fa-book"></i>Bonus 1: Conjugation guide</div>
-          <i class="fa-solid fa-square-plus"></i>`
-      }
-  })
+  const bonus2Menu = document.querySelector(".bonus2Menu");
+  const bonus2SubContainer = document.querySelector(".bonus2-subcontainer");
 
-  bonus2Menu.addEventListener("click", ()=>{
-    if(bonus2SubContainer.style.display === "none") {
-        bonus2SubContainer.style.display = "block";
-        bonus2Menu.innerHTML = `<div><i class='fa-solid fa-chart-line'></i>Bonus 2: 15 of the most common French verbs.</div>
-        <i class='fa-solid fa-circle-chevron-up'></i>`
+  const bonus3Menu = document.querySelector(".bonus3Menu");
+  const bonus3SubContainer = document.querySelector(".bonus3-subcontainer");
 
-        
+  const bonus4Menu = document.querySelector(".bonus4Menu");
+  const bonus4SubContainer = document.querySelector(".bonus4-subcontainer");
 
+  bonus1Menu.addEventListener("click", () => {
+    if (bonus1SubContainer.style.display === "none") {
+      bonus1SubContainer.style.display = "block";
+      bonus1Menu.innerHTML = `<div><i class="fa-solid fa-book"></i>Bonus 1: Conjugation guide</div>
+          <i class='fa-solid fa-circle-chevron-up'></i>`;
     } else {
-        bonus2SubContainer.style.display = "none";
-        bonus2Menu.innerHTML = `<div><i class='fa-solid fa-chart-line'></i>Bonus 2: 15 of the most common French verbs.</div>
-        <i class="fa-solid fa-square-plus"></i>`
+      bonus1SubContainer.style.display = "none";
+      bonus1Menu.innerHTML = `<div><i class="fa-solid fa-book"></i>Bonus 1: Conjugation guide</div>
+          <i class="fa-solid fa-square-plus"></i>`;
     }
-})
+  });
 
-  }
+  bonus2Menu.addEventListener("click", () => {
+    if (bonus2SubContainer.style.display === "none") {
+      bonus2SubContainer.style.display = "block";
+      bonus2Menu.innerHTML = `<div><i class='fa-solid fa-chart-line'></i>Bonus 2: 15 of the most common French verbs.</div>
+        <i class='fa-solid fa-circle-chevron-up'></i>`;
+    } else {
+      bonus2SubContainer.style.display = "none";
+      bonus2Menu.innerHTML = `<div><i class='fa-solid fa-chart-line'></i>Bonus 2: 15 of the most common French verbs.</div>
+        <i class="fa-solid fa-square-plus"></i>`;
+    }
+  });
+
+  //
+
+  bonus3Menu.addEventListener("click", () => {
+    if (bonus3SubContainer.style.display === "none") {
+      bonus3SubContainer.style.display = "block";
+      bonus3Menu.innerHTML = `<div><i class="fa-solid fa-fire"></i>Bonus 3: Unlock the Passé composé tense.</div>
+        <i class='fa-solid fa-circle-chevron-up'></i>`;
+    } else {
+      bonus3SubContainer.style.display = "none";
+      bonus3Menu.innerHTML = `<div><i class="fa-solid fa-fire"></i>Bonus 3: Unlock the Passé composé tense.</div>
+        <i class="fa-solid fa-square-plus"></i>`;
+    }
+  });
+
+  //
+
+  bonus4Menu.addEventListener("click", () => {
+    if (bonus4SubContainer.style.display === "none") {
+      bonus4SubContainer.style.display = "block";
+      bonus4Menu.innerHTML = `<div><i class="fa-solid fa-key"></i>Bonus 4: 15 of the most common French verbs (part 2).</div>
+        <i class='fa-solid fa-circle-chevron-up'></i>`;
+    } else {
+      bonus4SubContainer.style.display = "none";
+      bonus4Menu.innerHTML = `<div><i class="fa-solid fa-key"></i>Bonus 4: 15 of the most common French verbs (part 2).</div>
+        <i class="fa-solid fa-square-plus"></i>`;
+    }
+  });
+}
--- src/bonuses.js
+++ src/bonuses.js
@@ -54,7 +54,7 @@ export function generateBonusPage(userId, subStatus) {
 
   const bonus1IntroLine = document.createElement("div");
 
-  if ((userId && subStatus === "active") || streak >= 3) {
+  if ((userId && subStatus === "active") || streak >= 2) {
     bonus1IntroLine.innerHTML = `<i class="fa-solid fa-circle-chevron-right"></i>
       Your bonus is ready now!`;
 
@@ -77,12 +77,10 @@ export function generateBonusPage(userId, subStatus) {
       a.click();
 
       document.body.removeChild(a);
-
-      //window.open("../public/frcnjgd.pdf")
     });
   } else {
     bonus1IntroLine.innerHTML = `<i
-      class="fa-solid fa-lock"></i>Achieve a 3-day streak or become Premium to access it now`;
+      class="fa-solid fa-lock"></i>Achieve a 2-day streak or become Premium to access it now`;
     bonus1Intro.appendChild(bonus1IntroLine);
   }
 
@@ -118,7 +116,7 @@ export function generateBonusPage(userId, subStatus) {
 
   const bonus2IntroLine = document.createElement("div");
 
-  if ((userId && subStatus === "active") || streak >= 7) {
+  if ((userId && subStatus === "active") || streak >= 3) {
     bonus2IntroLine.innerHTML = `<i class="fa-solid fa-circle-chevron-right"></i>
       Your bonus is ready now!`;
 
@@ -144,11 +142,113 @@ export function generateBonusPage(userId, subStatus) {
     });
   } else {
     bonus2IntroLine.innerHTML = `<i
-      class="fa-solid fa-lock"></i>Achieve a 7-day streak or become Premium to access it now`;
+      class="fa-solid fa-lock"></i>Achieve a 3-day streak or become Premium to access it now`;
     bonus2Intro.appendChild(bonus2IntroLine);
   }
 
   bonus2SubContainer.appendChild(bonus2Intro);
+
+  // generate bonus 3
+  const bonus3Container = document.createElement("div");
+
+  menu.appendChild(bonus3Container);
+
+  const bonus3Menu = document.createElement("div");
+  bonus3Menu.classList.add("bonus3Menu");
+
+  bonus3Container.appendChild(bonus3Menu);
+
+  const bonus3MenuLine = document.createElement("div");
+  bonus3Menu.appendChild(bonus3MenuLine);
+
+  bonus3MenuLine.innerHTML = `<i class="fa-solid fa-fire"></i>Bonus 3: Unlock the Passé composé tense.`;
+
+  const plusIcon3 = document.createElement("i");
+  plusIcon3.classList.add("fa-solid", "fa-square-plus");
+
+  bonus3Menu.appendChild(plusIcon3);
+
+  const bonus3SubContainer = document.createElement("div");
+  bonus3SubContainer.classList.add("bonus-subcontainer", "bonus3-subcontainer");
+  bonus3SubContainer.style.display = "none";
+  bonus3Container.appendChild(bonus3SubContainer);
+
+  const bonus3Intro = document.createElement("div");
+  bonus3Intro.classList.add("bonus-intro");
+
+  const bonus3IntroLine = document.createElement("div");
+
+  if ((userId && subStatus === "active") || streak >= 5) {
+    bonus3IntroLine.innerHTML = `<i class="fa-solid fa-circle-chevron-right"></i>
+      You've unlocked the Passé composé tense!`;
+  } else {
+    bonus3IntroLine.innerHTML = `<i
+      class="fa-solid fa-lock"></i>Achieve a 5-day streak or become Premium to unlock it now`;
+  }
+  bonus3Intro.appendChild(bonus3IntroLine);
+  bonus3SubContainer.appendChild(bonus3Intro);
+
+  // generate bonus 4
+  const bonus4Container = document.createElement("div");
+
+  menu.appendChild(bonus4Container);
+
+  const bonus4Menu = document.createElement("div");
+  bonus4Menu.classList.add("bonus4Menu");
+
+  bonus4Container.appendChild(bonus4Menu);
+
+  const bonus4MenuLine = document.createElement("div");
+  bonus4Menu.appendChild(bonus4MenuLine);
+
+  bonus4MenuLine.innerHTML = `<i class="fa-solid fa-key"></i>Bonus 4: 15 of the most common French verbs (part 2).`;
+
+  const plusIcon4 = document.createElement("i");
+  plusIcon4.classList.add("fa-solid", "fa-square-plus");
+
+  bonus4Menu.appendChild(plusIcon4);
+
+  const bonus4SubContainer = document.createElement("div");
+  bonus4SubContainer.classList.add("bonus-subcontainer", "bonus4-subcontainer");
+  bonus4SubContainer.style.display = "none";
+  bonus4Container.appendChild(bonus4SubContainer);
+
+  const bonus4Intro = document.createElement("div");
+  bonus4Intro.classList.add("bonus-intro");
+
+  const bonus4IntroLine = document.createElement("div");
+
+  if ((userId && subStatus === "active") || streak >= 7) {
+    bonus4IntroLine.innerHTML = `<i class="fa-solid fa-circle-chevron-right"></i>
+      Your bonus is ready now!`;
+
+    const bonus4Btn = document.createElement("div");
+    bonus4Btn.classList.add("bonus4Btn");
+    bonus4Btn.textContent = "download";
+
+    bonus4Intro.appendChild(bonus4IntroLine);
+    bonus4Intro.appendChild(bonus4Btn);
+
+    bonus4Btn.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const a = document.createElement("a");
+      a.href = "../public/15-verbs-2.pdf";
+      a.setAttribute("download", "");
+
+      document.body.appendChild(a);
+
+      a.click();
+
+      document.body.removeChild(a);
+    });
+  } else {
+    bonus4IntroLine.innerHTML = `<i
+      class="fa-solid fa-lock"></i>Achieve a 7-day streak or become Premium to access it now`;
+    bonus4Intro.appendChild(bonus4IntroLine);
+  }
+
+  bonus4SubContainer.appendChild(bonus4Intro);
 
   manageBonusMenuClicks(userId, subStatus);
 }
